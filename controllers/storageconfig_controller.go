33b8e3c247119bbd6fc033f6aaab1eceaf6c9273
Vedant Mahabaleshwarkar <vmahabal@redhat.com>
2023-11-07 14:08:07 -0500

    removed deprecated "default bucket" key
    Signed-off-by: Vedant Mahabaleshwarkar <vmahabal@redhat.com>

--- controllers/storageconfig_controller.go
+++ controllers/storageconfig_controller.go
@@ -54,9 +54,6 @@ func newStorageSecret(dataConnectionSecretsList *corev1.SecretList) *corev1.Secr
 		dataConnectionElement["access_key_id"] = string(secret.Data["AWS_ACCESS_KEY_ID"])
 		dataConnectionElement["secret_access_key"] = string(secret.Data["AWS_SECRET_ACCESS_KEY"])
 		dataConnectionElement["endpoint_url"] = string(secret.Data["AWS_S3_ENDPOINT"])
-		// We add both "default_bucket" and "bucket" because kserve and modelmesh expect different keys for the same value
-		// Once upstream has reconciled to one common key, we should remove the other one.
-		dataConnectionElement["default_bucket"] = string(secret.Data["AWS_S3_BUCKET"])
 		dataConnectionElement["bucket"] = string(secret.Data["AWS_S3_BUCKET"])
 		dataConnectionElement["region"] = string(secret.Data["AWS_DEFAULT_REGION"])
 		if secret.Data["AWS_CA_BUNDLE"] != nil {
