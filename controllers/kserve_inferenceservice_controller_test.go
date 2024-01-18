4be8311773d715b7e51d2bd25725e5988fb82965
bartoszmajsak <bartosz.majsak@gmail.com>
2024-01-18 22:18:10 +0100

    fix: adds network policies to unblock traffic between components
    This solution is based on an initially proposed [script](https://github.com/bartoszmajsak/issue-work-logs/tree/main/RHOAIENG-1003), but with slightly modified namespace selectors.
    
    Additional changes:
    
    - bumps ginkgo to v2 across the tests
    - provides composed matcher to easily inspect nested structs
    - cleans up test setup for KServe
    - renames variables in reconcilers to reflect actual objects used
    
    Fixes https://issues.redhat.com/browse/RHOAIENG-1955

--- controllers/kserve_inferenceservice_controller_test.go
+++ controllers/kserve_inferenceservice_controller_test.go
@@ -21,48 +21,59 @@ import (
 	kservev1beta1 "github.com/kserve/kserve/pkg/apis/serving/v1beta1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
+	gomegatypes "github.com/onsi/gomega/types"
 	"github.com/opendatahub-io/odh-model-controller/controllers/constants"
 	routev1 "github.com/openshift/api/route/v1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"knative.dev/pkg/apis"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"strings"
 	"time"
 )
 
 var _ = Describe("The Openshift Kserve model controller", func() {
 
 	When("creating a Kserve ServiceRuntime & InferenceService", func() {
+		var testNs string
+
 		BeforeEach(func() {
 			ctx := context.Background()
-			istioNamespace := &corev1.Namespace{
+			testNs = appendRandomNameTo("test-namespace")
+			testNamespace := &corev1.Namespace{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      constants.IstioNamespace,
-					Namespace: constants.IstioNamespace,
+					Name:      testNs,
+					Namespace: testNs,
 				},
 			}
-			Expect(cli.Create(ctx, istioNamespace)).Should(Succeed())
+			Expect(cli.Create(ctx, testNamespace)).Should(Succeed())
 
 			inferenceServiceConfig := &corev1.ConfigMap{}
-			err := convertToStructuredResource(InferenceServiceConfigPath1, inferenceServiceConfig)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(cli.Create(ctx, inferenceServiceConfig)).Should(Succeed())
+			Expect(convertToStructuredResource(InferenceServiceConfigPath1, inferenceServiceConfig)).To(Succeed())
+			if err := cli.Create(ctx, inferenceServiceConfig); err != nil && !errors.IsAlreadyExists(err) {
+				Fail(err.Error())
+			}
 
 			servingRuntime := &kservev1alpha1.ServingRuntime{}
-			err = convertToStructuredResource(KserveServingRuntimePath1, servingRuntime)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(cli.Create(ctx, servingRuntime)).Should(Succeed())
-
+			Expect(convertToStructuredResource(KserveServingRuntimePath1, servingRuntime)).To(Succeed())
+			if err := cli.Create(ctx, servingRuntime); err != nil && !errors.IsAlreadyExists(err) {
+				Fail(err.Error())
+			}
 		})
 
 		It("With Kserve InferenceService a Route be created", func() {
 			inferenceService := &kservev1beta1.InferenceService{}
 			err := convertToStructuredResource(KserveInferenceServicePath1, inferenceService)
 			Expect(err).NotTo(HaveOccurred())
+			inferenceService.SetNamespace(testNs)
+
 			Expect(cli.Create(ctx, inferenceService)).Should(Succeed())
 
 			By("By checking that the controller has not created the Route")
-			//time.Sleep(5000 * time.Millisecond)
 			Consistently(func() error {
 				route := &routev1.Route{}
 				key := types.NamespacedName{Name: getKServeRouteName(inferenceService), Namespace: constants.IstioNamespace}
@@ -87,10 +98,63 @@ var _ = Describe("The Openshift Kserve model controller", func() {
 				key := types.NamespacedName{Name: getKServeRouteName(inferenceService), Namespace: constants.IstioNamespace}
 				err = cli.Get(ctx, key, route)
 				return err
-			}, timeout, interval).ShouldNot(HaveOccurred())
+			}, timeout, interval).Should(Succeed())
 		})
+
+		It("should create required network policies when KServe is used", func() {
+			// given
+			inferenceService := &kservev1beta1.InferenceService{}
+			Expect(convertToStructuredResource(KserveInferenceServicePath1, inferenceService)).To(Succeed())
+			inferenceService.SetNamespace(testNs)
+
+			// when
+			Expect(cli.Create(ctx, inferenceService)).Should(Succeed())
+
+			// then
+			By("ensuring that the controller has created required network policies")
+			networkPolicies := &v1.NetworkPolicyList{}
+			Eventually(func() []v1.NetworkPolicy {
+				err := cli.List(ctx, networkPolicies, client.InNamespace(inferenceService.Namespace))
+				if err != nil {
+					Fail(err.Error())
+				}
+				return networkPolicies.Items
+			}, time.Second*1, interval).Should(HaveLen(3))
+
+			Expect(networkPolicies.Items).To(
+				ContainElements(
+					withMatchingNestedField("ObjectMeta.Name", Equal("allow-from-openshift-monitoring-ns")),
+					withMatchingNestedField("ObjectMeta.Name", Equal("allow-openshift-ingress")),
+					withMatchingNestedField("ObjectMeta.Name", Equal("allow-from-opendatahub-ns")),
+				),
+			)
+		})
+
 	})
 })
+
+func withMatchingNestedField(path string, matcher gomegatypes.GomegaMatcher) gomegatypes.GomegaMatcher {
+	if path == "" {
+		Fail("cannot handle empty path")
+	}
+
+	fields := strings.Split(path, ".")
+
+	// Reverse the path, so we start composing matchers from the leaf up
+	for i, j := 0, len(fields)-1; i < j; i, j = i+1, j-1 {
+		fields[i], fields[j] = fields[j], fields[i]
+	}
+
+	matchFields := MatchFields(IgnoreExtras,
+		Fields{fields[0]: matcher},
+	)
+
+	for i := 1; i < len(fields); i++ {
+		matchFields = MatchFields(IgnoreExtras, Fields{fields[i]: matchFields})
+	}
+
+	return matchFields
+}
 
 func getKServeRouteName(isvc *kservev1beta1.InferenceService) string {
 	return isvc.Name + "-" + isvc.Namespace
