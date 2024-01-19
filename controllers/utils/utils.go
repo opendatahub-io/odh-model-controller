03c6fb83765e9f4909b559a15e97085a88a4d0c2
bartoszmajsak <bartosz.majsak@gmail.com>
2024-01-19 10:50:20 +0100

    chore: uses KServeNamespace var instead of custom func+struct field

--- controllers/utils/utils.go
+++ controllers/utils/utils.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	kservev1beta1 "github.com/kserve/kserve/pkg/apis/serving/v1beta1"
-	"os"
 	"reflect"
 )
 
@@ -10,21 +9,6 @@ const (
 	inferenceServiceDeploymentModeAnnotation      = "serving.kserve.io/deploymentMode"
 	inferenceServiceDeploymentModeAnnotationValue = "ModelMesh"
 )
-
-// GetApplicationNamespace determines namespace in which this controller is deployed.
-func GetApplicationNamespace() (string, error) {
-	ns, found := os.LookupEnv("POD_NAMESPACE")
-	if !found {
-		namespace, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
-		if err != nil {
-			return "", nil
-		}
-
-		return string(namespace), err
-	}
-
-	return ns, nil
-}
 
 func IsDeploymentModeForIsvcModelMesh(isvc *kservev1beta1.InferenceService) bool {
 	value, exists := isvc.Annotations[inferenceServiceDeploymentModeAnnotation]
