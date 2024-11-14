a0c3ec494d0d75e87d3874a838e16380c40849c2
Tomer Figenblat <tomer.figenblat@gmail.com>
2024-11-14 13:26:02 -0500

    Task/NVPE-10: Create NIM Account controller (#289)
    * feat: added nim account controller
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * test: added tests for the new nim account controller
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * chore: fixed change request, owned resources are deleted by k8s gc
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * chore: fixed change request, rearrange status conditions reporting
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * chore: fixed change request, use server side apply where applicable
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * docs: clarified code docs
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * chore: switched to utility func for gvk fetch
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * chore: added count to utility script
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * fix: cleanup resources if failed to validate the api key
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * test: fixed failing ci test (worked locally)
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * fix: added pagesize to catalog query
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * fix: added missing rbac for account status
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * chore: fixed change request, clarify variable name
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * fix: intial account stauts should be unknown
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * chore: fixed various small change request
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * fix: several fixes and requests
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * test: fixed flaky test
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * fix: missing finalizer permissions
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * fix: missing create perms for templates
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * test: made test more reliable
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * test: fixed flaky test by increasing timeout
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * fix: wrong type of pull secret
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * test: nim account should be created in seperated namespace per testcases
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    ---------
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ import (
 	"github.com/opendatahub-io/odh-model-controller/controllers/webhook"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
 	knservingv1 "knative.dev/serving/pkg/apis/serving/v1"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
@@ -86,6 +87,10 @@ func init() { //nolint:gochecknoinits //reason this way we ensure schemes are al
 // +kubebuilder:rbac:groups=authorino.kuadrant.io,resources=authconfigs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=datasciencecluster.opendatahub.io,resources=datascienceclusters,verbs=get;list;watch
 // +kubebuilder:rbac:groups=dscinitialization.opendatahub.io,resources=dscinitializations,verbs=get;list;watch
+// +kubebuilder:rbac:groups=nim.opendatahub.io,resources=accounts,verbs=get;list;watch;update
+// +kubebuilder:rbac:groups=nim.opendatahub.io,resources=accounts/status,verbs=get;list;watch;update
+// +kubebuilder:rbac:groups=nim.opendatahub.io,resources=accounts/finalizers,verbs=update
+// +kubebuilder:rbac:groups=template.openshift.io,resources=templates,verbs=get;list;watch;create;update;delete
 
 func getEnvAsBool(name string, defaultValue bool) bool {
 	valStr := os.Getenv(name)
@@ -120,7 +125,8 @@ func main() {
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	cfg := ctrl.GetConfigOrDie()
+	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
 		Scheme: scheme,
 		Metrics: server.Options{
 			BindAddress: metricsAddr,
@@ -220,7 +226,23 @@ func main() {
 		setupLog.Info("Skipping setup of Knative Service validating/mutating Webhook, because KServe Serverless setup seems to be disabled in the DataScienceCluster resource.")
 	}
 
-	if err := builder.WebhookManagedBy(mgr).
+	kclient, kcErr := kubernetes.NewForConfig(cfg)
+	if kcErr != nil {
+		setupLog.Error(err, "unable to create clientset")
+		os.Exit(1)
+	}
+
+	ctx := ctrl.SetupSignalHandler()
+	if err = (&controllers.NimAccountReconciler{
+		Client:  mgr.GetClient(),
+		Log:     ctrl.Log.WithName("controllers").WithName("NimAccountReconciler"),
+		KClient: kclient,
+	}).SetupWithManager(mgr, ctx); err != nil {
+		setupLog.Error(err, "unable to create controller NIM Account controller")
+		os.Exit(1)
+	}
+
+	if err = builder.WebhookManagedBy(mgr).
 		For(&nimv1.Account{}).
 		WithValidator(webhook.NewNimAccountValidator(mgr.GetClient())).
 		Complete(); err != nil {
@@ -241,7 +263,7 @@ func main() {
 	}
 
 	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	if err := mgr.Start(ctx); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
