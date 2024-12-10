4e37af0fcd3ef85fff4b22565ddd4bcc02ddf2b0
Edgar Hernández <23639005+israel-hdez@users.noreply.github.com>
2024-12-10 17:11:35 -0600

    Implementation of ODH defaulters for InferenceGraph and InferenceService
    On creation of InferenceGraph or InferenceService resources, the following default annotations will be added:
    * `serving.knative.openshift.io/enablePassthrough: true`
    * `sidecar.istio.io/inject: true`
    * `sidecar.istio.io/rewriteAppHTTPProbers: true`
    
    The annotations are added only for Serverless mode, and only if they are missing.
    
    Signed-off-by: Edgar Hernández <23639005+israel-hdez@users.noreply.github.com>

--- cmd/main.go
+++ cmd/main.go
@@ -48,6 +48,7 @@ import (
 	"github.com/opendatahub-io/odh-model-controller/internal/controller/utils"
 	webhooknimv1 "github.com/opendatahub-io/odh-model-controller/internal/webhook/nim/v1"
 	webhookservingv1 "github.com/opendatahub-io/odh-model-controller/internal/webhook/serving/v1"
+	webhookservingv1alpha1 "github.com/opendatahub-io/odh-model-controller/internal/webhook/serving/v1alpha1"
 	webhookservingv1beta1 "github.com/opendatahub-io/odh-model-controller/internal/webhook/serving/v1beta1"
 	// +kubebuilder:scaffold:imports
 )
@@ -269,6 +270,13 @@ func main() {
 	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
 		if err = webhookservingv1beta1.SetupInferenceServiceWebhookWithManager(mgr); err != nil {
 			setupLog.Error(err, "unable to create webhook", "webhook", "InferenceService")
+			os.Exit(1)
+		}
+	}
+	// nolint:goconst
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = webhookservingv1alpha1.SetupInferenceGraphWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "InferenceGraph")
 			os.Exit(1)
 		}
 	}
