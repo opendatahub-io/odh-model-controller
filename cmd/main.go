ba421ec6f2edb7b35e608acf1fd769ddc3e6724e
Edgar Hernández <ehernand@redhat.com>
2025-01-28 16:55:50 -0600

    Prevent InferenceGraphs to refer to other namespaces (#357)
    In ODH we prefer isolation between projects/namespaces. For InferenceGraph CRD, KServe doesn't do any validation on the used hostname when a node/step is using `serviceUrl`. This can break isolation between projects, because it is allowed to specify a hostname from another namespace.
    
    This adds a validating webhook to prevent the user from using an in-cluster hostname that would be referring to a different namespace from the created/updated InferenceGraph.
    
    Signed-off-by: Edgar Hernández <23639005+israel-hdez@users.noreply.github.com>

--- cmd/main.go
+++ cmd/main.go
@@ -35,7 +35,6 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-
 	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
