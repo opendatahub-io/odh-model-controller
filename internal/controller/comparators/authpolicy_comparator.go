d34bbe3f882cd4548aa3d52e43fab68c4d3b38d0
Jooho Lee <jlee@redhat.com>
2025-09-19 02:11:36 -0400

    feat: add authPolicy comparator (#515)
    * add authPolicy comparator
    
    Signed-off-by: Jooho Lee <jlee@redhat.com>
    
    * Add comprehensive test cases for AuthPolicy comparator and update compare logic
    
    Signed-off-by: Jooho Lee <jlee@redhat.com>
    
    * remove unnecessary parts and fix comparator
    
    Signed-off-by: Jooho Lee <jlee@redhat.com>
    
    * fix lint
    
    Signed-off-by: Jooho Lee <jlee@redhat.com>
    
    ---------
    
    Signed-off-by: Jooho Lee <jlee@redhat.com>

---
+++ internal/controller/comparators/authpolicy_comparator.go
@@ -0,0 +1,33 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package comparators
+
+import (
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func GetAuthPolicyComparator() ResourceComparator {
+	return func(current client.Object, desired client.Object) bool {
+		currentAP := current.(*unstructured.Unstructured)
+		desiredAP := desired.(*unstructured.Unstructured)
+
+		return equality.Semantic.DeepDerivative(desiredAP.Object["spec"], currentAP.Object["spec"]) &&
+			equality.Semantic.DeepDerivative(desiredAP.GetLabels(), currentAP.GetLabels()) &&
+			equality.Semantic.DeepDerivative(desiredAP.GetAnnotations(), currentAP.GetAnnotations())
+	}
+}
---
+++ internal/controller/comparators/authpolicy_comparator_test.go
@@ -0,0 +1,294 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package comparators_test
+
+import (
+	"testing"
+
+	"github.com/opendatahub-io/odh-model-controller/internal/controller/comparators"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func createAuthPolicy(appLabel string) *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "kuadrant.io/v1",
+			"kind":       "AuthPolicy",
+			"metadata": map[string]interface{}{
+				"name":      "test-authn",
+				"namespace": "test-ns",
+				"labels": map[string]interface{}{
+					"app": appLabel,
+				},
+			},
+			"spec": map[string]interface{}{
+				"defaults": map[string]interface{}{
+					"strategy": "atomic",
+				},
+			},
+		},
+	}
+}
+
+func createAuthPolicyWithExtraLabels(appLabel string) *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "kuadrant.io/v1",
+			"kind":       "AuthPolicy",
+			"metadata": map[string]interface{}{
+				"name":      "test-authn",
+				"namespace": "test-ns",
+				"labels": map[string]interface{}{
+					"app": appLabel,
+					"kubectl.kubernetes.io/last-applied-configuration": "...",
+					"app.kubernetes.io/managed-by":                     "controller",
+				},
+			},
+			"spec": map[string]interface{}{
+				"defaults": map[string]interface{}{
+					"strategy": "atomic",
+				},
+			},
+		},
+	}
+}
+
+func createAuthPolicyWithoutLabels() *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "kuadrant.io/v1",
+			"kind":       "AuthPolicy",
+			"metadata": map[string]interface{}{
+				"name":      "test-authn",
+				"namespace": "test-ns",
+			},
+			"spec": map[string]interface{}{
+				"defaults": map[string]interface{}{
+					"strategy": "atomic",
+				},
+			},
+		},
+	}
+}
+
+func createAuthPolicyWithAnnotations(annotations map[string]string) *unstructured.Unstructured {
+	annotationsInterface := make(map[string]interface{})
+	for k, v := range annotations {
+		annotationsInterface[k] = v
+	}
+
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "kuadrant.io/v1",
+			"kind":       "AuthPolicy",
+			"metadata": map[string]interface{}{
+				"name":      "test-authn",
+				"namespace": "test-ns",
+				"labels": map[string]interface{}{
+					"app": "test",
+				},
+				"annotations": annotationsInterface,
+			},
+			"spec": map[string]interface{}{
+				"defaults": map[string]interface{}{
+					"strategy": "atomic",
+				},
+			},
+		},
+	}
+}
+
+func createComplexAuthPolicy(gatewayName, authType string) *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "kuadrant.io/v1",
+			"kind":       "AuthPolicy",
+			"metadata": map[string]interface{}{
+				"name":      "llm-gateway-authn",
+				"namespace": "openshift-ingress",
+				"labels": map[string]interface{}{
+					"app": "test",
+				},
+			},
+			"spec": map[string]interface{}{
+				"targetRef": map[string]interface{}{
+					"group": "gateway.networking.k8s.io",
+					"kind":  "Gateway",
+					"name":  gatewayName,
+				},
+				"defaults": map[string]interface{}{
+					"rules": map[string]interface{}{
+						"authentication": map[string]interface{}{
+							"kubernetes-user": map[string]interface{}{
+								"credentials": map[string]interface{}{
+									"authorizationHeader": map[string]interface{}{},
+								},
+								"kubernetesTokenReview": map[string]interface{}{
+									"audiences": []interface{}{
+										"https://example.com/test-audience",
+									},
+								},
+							},
+						},
+						"authorization": map[string]interface{}{
+							authType: map[string]interface{}{
+								"kubernetesSubjectAccessReview": map[string]interface{}{
+									"user": map[string]interface{}{
+										"expression": "auth.identity.user.username",
+									},
+									"authorizationGroups": map[string]interface{}{
+										"expression": "auth.identity.user.groups",
+									},
+									"resourceAttributes": map[string]interface{}{
+										"group": map[string]interface{}{
+											"value": "serving.kserve.io",
+										},
+										"resource": map[string]interface{}{
+											"value": "llminferenceservices",
+										},
+										"namespace": map[string]interface{}{
+											"expression": "request.path.split(\"/\")[1]",
+										},
+										"name": map[string]interface{}{
+											"expression": "request.path.split(\"/\")[2]",
+										},
+										"verb": map[string]interface{}{
+											"value": "get",
+										},
+									},
+								},
+								"priority": 1,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func TestAuthPolicyComparator(t *testing.T) {
+	comparator := comparators.GetAuthPolicyComparator()
+
+	tests := []struct {
+		name      string
+		deployed  *unstructured.Unstructured
+		requested *unstructured.Unstructured
+		expected  bool
+	}{
+		{
+			name:      "identical AuthPolicies should return true",
+			deployed:  createAuthPolicy("test"),
+			requested: createAuthPolicy("test"),
+			expected:  true,
+		},
+		{
+			name:      "different labels should return false",
+			deployed:  createAuthPolicy("test"),
+			requested: createAuthPolicy("different"),
+			expected:  false,
+		},
+		{
+			name:      "policies without labels should return true when specs match",
+			deployed:  createAuthPolicyWithoutLabels(),
+			requested: createAuthPolicyWithoutLabels(),
+			expected:  true,
+		},
+		{
+			name:      "deployed with extra labels - DeepDerivative checks if requested is subset of deployed",
+			deployed:  createAuthPolicyWithExtraLabels("test"),
+			requested: createAuthPolicy("test"),
+			expected:  true,
+		},
+		{
+			name:      "requested with extra labels - deployed cannot contain all requested fields",
+			deployed:  createAuthPolicy("test"),
+			requested: createAuthPolicyWithExtraLabels("test"),
+			expected:  false,
+		},
+		{
+			name:      "complex AuthPolicy with different authorization types should return false",
+			deployed:  createComplexAuthPolicy("openshift-ai-inference", "tier-access"),
+			requested: createComplexAuthPolicy("openshift-ai-inference", "admin-access"),
+			expected:  false,
+		},
+		{
+			name:      "complex AuthPolicy with identical structure should return true",
+			deployed:  createComplexAuthPolicy("openshift-ai-inference", "tier-access"),
+			requested: createComplexAuthPolicy("openshift-ai-inference", "tier-access"),
+			expected:  true,
+		},
+		{
+			name:      "complex AuthPolicy with different gateway names should return false",
+			deployed:  createComplexAuthPolicy("gateway-1", "tier-access"),
+			requested: createComplexAuthPolicy("gateway-2", "tier-access"),
+			expected:  false,
+		},
+		{
+			name: "policies with identical annotations should return true",
+			deployed: createAuthPolicyWithAnnotations(map[string]string{
+				"example.com/annotation": "value1",
+				"test.io/config":         "enabled",
+			}),
+			requested: createAuthPolicyWithAnnotations(map[string]string{
+				"example.com/annotation": "value1",
+				"test.io/config":         "enabled",
+			}),
+			expected: true,
+		},
+		{
+			name: "policies with different annotation values should return false",
+			deployed: createAuthPolicyWithAnnotations(map[string]string{
+				"example.com/annotation": "value1",
+			}),
+			requested: createAuthPolicyWithAnnotations(map[string]string{
+				"example.com/annotation": "value2",
+			}),
+			expected: false,
+		},
+		{
+			name: "deployed with extra annotations - DeepDerivative checks if requested is subset of deployed",
+			deployed: createAuthPolicyWithAnnotations(map[string]string{
+				"example.com/annotation":             "value1",
+				"kubectl.kubernetes.io/last-applied": "...",
+			}),
+			requested: createAuthPolicyWithAnnotations(map[string]string{
+				"example.com/annotation": "value1",
+			}),
+			expected: true,
+		},
+		{
+			name: "requested with extra annotations - deployed cannot contain all requested fields",
+			deployed: createAuthPolicyWithAnnotations(map[string]string{
+				"example.com/annotation": "value1",
+			}),
+			requested: createAuthPolicyWithAnnotations(map[string]string{
+				"example.com/annotation":             "value1",
+				"kubectl.kubernetes.io/last-applied": "...",
+			}),
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := comparator(tt.deployed, tt.requested)
+			if result != tt.expected {
+				t.Errorf("comparator() = %v, expected %v", result, tt.expected)
+			}
+		})
+	}
+}
