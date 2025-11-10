30a44dea8245777bcaf346b7b85a7dd765f6497c
Vedant Mahabaleshwarkar <vmahabal@redhat.com>
2025-11-10 16:28:35 -0500

    resolve race condition in raw crb when 2 isvcs using the same SA have different auth requirements
    Signed-off-by: Vedant Mahabaleshwarkar <vmahabal@redhat.com>

--- internal/controller/serving/reconcilers/kserve_raw_clusterrolebinding_reconciler.go
+++ internal/controller/serving/reconcilers/kserve_raw_clusterrolebinding_reconciler.go
@@ -50,7 +50,10 @@ func NewKserveRawClusterRoleBindingReconciler(client client.Client) *KserveRawCl
 func (r *KserveRawClusterRoleBindingReconciler) Reconcile(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) error {
 	log.V(1).Info("Reconciling ClusterRoleBinding for InferenceService")
 	// Create Desired resource
-	desiredResource := r.createDesiredResource(isvc)
+	desiredResource, err := r.createDesiredResource(ctx, log, isvc)
+	if err != nil {
+		return err
+	}
 
 	// Get Existing resource
 	existingResource, err := r.getExistingResource(ctx, log, isvc)
@@ -66,38 +69,13 @@ func (r *KserveRawClusterRoleBindingReconciler) Reconcile(ctx context.Context, l
 }
 
 func (r *KserveRawClusterRoleBindingReconciler) Delete(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) error {
-
-	var isvcList kservev1beta1.InferenceServiceList
-	var listOpts client.ListOptions
-	namespace := isvc.Namespace
-	listOpts = client.ListOptions{Namespace: namespace}
-	// List all inference services in the namespace
-	if err := r.client.List(ctx, &isvcList, &listOpts); err != nil {
-		log.Error(err, "failed to list inference services")
-		return err
-	}
-
-	hasCustomSA := false
 	isvcSA := r.serviceAccountName
 	if len(isvc.Spec.Predictor.ServiceAccountName) > 0 {
-		hasCustomSA = true
 		isvcSA = isvc.Spec.Predictor.ServiceAccountName
 	}
-	var existingIsvcs []kservev1beta1.InferenceService
-	for _, svc := range isvcList.Items {
-		if hasCustomSA {
-			if svc.Spec.Predictor.ServiceAccountName == isvcSA {
-				if svc.GetDeletionTimestamp() == nil {
-					existingIsvcs = append(existingIsvcs, svc)
-				}
-			}
-		} else {
-			if len(svc.Spec.Predictor.ServiceAccountName) == 0 {
-				if svc.GetDeletionTimestamp() == nil {
-					existingIsvcs = append(existingIsvcs, svc)
-				}
-			}
-		}
+	existingIsvcs, err := getExistingIsvcsWithTheSameSAandAuthEnabled(ctx, r.client, log, isvcSA, isvc.Namespace)
+	if err != nil {
+		return err
 	}
 	if len(existingIsvcs) == 0 {
 		crbName := r.clusterRoleBindingHandler.GetClusterRoleBindingName(isvc.Namespace, isvcSA)
@@ -107,18 +85,28 @@ func (r *KserveRawClusterRoleBindingReconciler) Delete(ctx context.Context, log 
 	return nil
 }
 
-func (r *KserveRawClusterRoleBindingReconciler) createDesiredResource(isvc *kservev1beta1.InferenceService) *v1.ClusterRoleBinding {
-	if val, ok := isvc.Annotations[constants.EnableAuthODHAnnotation]; !ok || val != "true" {
-		return nil
-	}
-
+func (r *KserveRawClusterRoleBindingReconciler) createDesiredResource(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) (*v1.ClusterRoleBinding, error) {
 	isvcSA := r.serviceAccountName
 	if isvc.Spec.Predictor.ServiceAccountName != "" {
 		isvcSA = isvc.Spec.Predictor.ServiceAccountName
 	}
+
+	if val, ok := isvc.Annotations[constants.EnableAuthODHAnnotation]; !ok || val != "true" {
+		existingIsvcs, err := getExistingIsvcsWithTheSameSAandAuthEnabled(ctx, r.client, log, isvcSA, isvc.Namespace)
+		if err != nil {
+			return nil, err
+		}
+		if len(existingIsvcs) == 0 {
+			// This ISVC does not request auth and no other ISVC using the same SA requests auth, so no CRB is needed
+			return nil, nil
+		}
+		// if execution has reached here, it means at least one other ISVC using the same SA requests auth
+		// thus, CRB is needed
+	}
+
 	desiredClusterRoleBindingName := r.clusterRoleBindingHandler.GetClusterRoleBindingName(isvc.Namespace, isvcSA)
 	desiredClusterRoleBinding := r.clusterRoleBindingHandler.CreateDesiredClusterRoleBinding(desiredClusterRoleBindingName, isvcSA, isvc.Namespace)
-	return desiredClusterRoleBinding
+	return desiredClusterRoleBinding, nil
 }
 
 func (r *KserveRawClusterRoleBindingReconciler) getExistingResource(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) (*v1.ClusterRoleBinding, error) {
@@ -137,4 +125,28 @@ func (r *KserveRawClusterRoleBindingReconciler) processDelta(ctx context.Context
 func (r *KserveRawClusterRoleBindingReconciler) Cleanup(_ context.Context, _ logr.Logger, _ string) error {
 	// NO OP
 	return nil
+}
+
+func getExistingIsvcsWithTheSameSAandAuthEnabled(ctx context.Context, cli client.Client, log logr.Logger, isvcSA string, namespace string) ([]kservev1beta1.InferenceService, error) {
+	var isvcList kservev1beta1.InferenceServiceList
+	listOpts := client.ListOptions{Namespace: namespace}
+	// List all inference services in the namespace
+	if err := cli.List(ctx, &isvcList, &listOpts); err != nil {
+		log.Error(err, "failed to list inference services")
+		return nil, err
+	}
+	var existingIsvcs []kservev1beta1.InferenceService
+	for _, svc := range isvcList.Items {
+		if svc.GetDeletionTimestamp() != nil {
+			continue
+		}
+		if val, ok := svc.Annotations[constants.EnableAuthODHAnnotation]; ok && val == "true" {
+			if isvcSA == constants.KserveServiceAccountName && len(svc.Spec.Predictor.ServiceAccountName) == 0 {
+				existingIsvcs = append(existingIsvcs, svc)
+			} else if isvcSA == svc.Spec.Predictor.ServiceAccountName {
+				existingIsvcs = append(existingIsvcs, svc)
+			}
+		}
+	}
+	return existingIsvcs, nil
 }
