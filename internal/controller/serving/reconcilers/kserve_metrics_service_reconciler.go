opendatahub-io/odh-model-controller

commit 203969f36fa95182e90608a74206a3df3b38efc0

[RHOAIENG-10293] add metrics resources for rawdeployment (#347)
* [RHOAIENG-10293] add metrics resources for rawdeployment

Signed-off-by: Vedant Mahabaleshwarkar <vmahabal@redhat.com>

* [RHOAIENG-10293] address feedback

Signed-off-by: Vedant Mahabaleshwarkar <vmahabal@redhat.com>

---------

Signed-off-by: Vedant Mahabaleshwarkar <vmahabal@redhat.com>

internal/controller/serving/reconcilers/kserve_metrics_service_reconciler.go
@@ -17,11 +17,18 @@ package reconcilers
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/go-logr/logr"
 	kservev1beta1 "github.com/kserve/kserve/pkg/apis/serving/v1beta1"
+	"github.com/kserve/kserve/pkg/constants"
+	constants2 "github.com/opendatahub-io/odh-model-controller/internal/controller/constants"
+	"github.com/opendatahub-io/odh-model-controller/internal/controller/utils"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/opendatahub-io/odh-model-controller/internal/controller/comparators"
@@ -29,29 +36,28 @@ import (
 	"github.com/opendatahub-io/odh-model-controller/internal/controller/resources"
 )
 
-var _ SubResourceReconciler = (*KserveMetricsServiceReconciler)(nil)
+var _ SubResourceReconciler = (*KserveRawMetricsServiceReconciler)(nil)
 
-type KserveMetricsServiceReconciler struct {
+type KserveRawMetricsServiceReconciler struct {
 	NoResourceRemoval
 	client         client.Client
 	serviceHandler resources.ServiceHandler
 	deltaProcessor processors.DeltaProcessor
 }
 
-func NewKServeMetricsServiceReconciler(client client.Client) *KserveMetricsServiceReconciler {
-	return &KserveMetricsServiceReconciler{
+func NewKServeRawMetricsServiceReconciler(client client.Client) *KserveRawMetricsServiceReconciler {
+	return &KserveRawMetricsServiceReconciler{
 		client:         client,
 		serviceHandler: resources.NewServiceHandler(client),
 		deltaProcessor: processors.NewDeltaProcessor(),
 	}
 }
 
-// TODO remove this reconcile loop in future versions
-func (r *KserveMetricsServiceReconciler) Reconcile(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) error {
+func (r *KserveRawMetricsServiceReconciler) Reconcile(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) error {
 	log.V(1).Info("Reconciling Metrics Service for InferenceService, checking if there are resource for deletion")
 
 	// Create Desired resource
-	desiredResource, err := r.createDesiredResource(log, isvc)
+	desiredResource, err := r.createDesiredResource(ctx, log, isvc)
 	if err != nil {
 		return err
 	}
@@ -69,15 +75,57 @@ func (r *KserveMetricsServiceReconciler) Reconcile(ctx context.Context, log logr
 	return nil
 }
 
-func (r *KserveMetricsServiceReconciler) createDesiredResource(log logr.Logger, isvc *kservev1beta1.InferenceService) (*v1.Service, error) {
-	return nil, nil
+func (r *KserveRawMetricsServiceReconciler) createDesiredResource(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) (*v1.Service, error) {
+	isvcRuntime, err := utils.FindSupportingRuntimeForISvc(ctx, r.client, log, isvc)
+	if err != nil {
+		return nil, err
+	}
+
+	if isvcRuntime.Spec.Annotations == nil || isvcRuntime.Spec.Annotations[constants.PrometheusPortAnnotationKey] == "" {
+		log.V(1).Info("No Prometheus annotations on ServingRuntime, skipping creation of metrics resources")
+		return nil, nil
+	}
+
+	prometheusPortAnnotationValue := isvcRuntime.Spec.Annotations[constants.PrometheusPortAnnotationKey]
+	prometheusPort, err := strconv.ParseInt(prometheusPortAnnotationValue, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	metricsService := &v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      getMetricsServiceName(isvc),
+			Namespace: isvc.Namespace,
+			Labels: map[string]string{
+				"name": getMetricsServiceName(isvc),
+			},
+		},
+		Spec: v1.ServiceSpec{
+			Ports: []v1.ServicePort{
+				{
+					Name:       isvcRuntime.Name + "-metrics",
+					Protocol:   v1.ProtocolTCP,
+					Port:       int32(prometheusPort),
+					TargetPort: intstr.FromInt32(int32(prometheusPort)),
+				},
+			},
+			Type: v1.ServiceTypeClusterIP,
+			Selector: map[string]string{
+				constants2.KserveGroupAnnotation: isvc.Name,
+			},
+		},
+	}
+	if err := ctrl.SetControllerReference(isvc, metricsService, r.client.Scheme()); err != nil {
+		log.Error(err, "Unable to add OwnerReference to the Metrics Service")
+		return nil, err
+	}
+	return metricsService, nil
 }
 
-func (r *KserveMetricsServiceReconciler) getExistingResource(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) (*v1.Service, error) {
+func (r *KserveRawMetricsServiceReconciler) getExistingResource(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) (*v1.Service, error) {
 	return r.serviceHandler.FetchService(ctx, log, types.NamespacedName{Name: getMetricsServiceName(isvc), Namespace: isvc.Namespace})
 }
 
-func (r *KserveMetricsServiceReconciler) processDelta(ctx context.Context, log logr.Logger, desiredService *v1.Service, existingService *v1.Service) (err error) {
+func (r *KserveRawMetricsServiceReconciler) processDelta(ctx context.Context, log logr.Logger, desiredService *v1.Service, existingService *v1.Service) (err error) {
 	comparator := comparators.GetServiceComparator()
 	delta := r.deltaProcessor.ComputeDelta(comparator, desiredService, existingService)
 

internal/controller/serving/reconcilers/kserve_metrics_servicemonitor_reconciler.go
@@ -20,8 +20,11 @@ import (
 
 	"github.com/go-logr/logr"
 	kservev1beta1 "github.com/kserve/kserve/pkg/apis/serving/v1beta1"
+	"github.com/opendatahub-io/odh-model-controller/internal/controller/utils"
 	v1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/opendatahub-io/odh-model-controller/internal/controller/comparators"
@@ -29,29 +32,28 @@ import (
 	"github.com/opendatahub-io/odh-model-controller/internal/controller/resources"
 )
 
-var _ SubResourceReconciler = (*KserveMetricsServiceMonitorReconciler)(nil)
+var _ SubResourceReconciler = (*KserveRawMetricsServiceMonitorReconciler)(nil)
 
-type KserveMetricsServiceMonitorReconciler struct {
+type KserveRawMetricsServiceMonitorReconciler struct {
 	NoResourceRemoval
 	client                client.Client
 	serviceMonitorHandler resources.ServiceMonitorHandler
 	deltaProcessor        processors.DeltaProcessor
 }
 
-func NewKServeMetricsServiceMonitorReconciler(client client.Client) *KserveMetricsServiceMonitorReconciler {
-	return &KserveMetricsServiceMonitorReconciler{
+func NewKServeRawMetricsServiceMonitorReconciler(client client.Client) *KserveRawMetricsServiceMonitorReconciler {
+	return &KserveRawMetricsServiceMonitorReconciler{
 		client:                client,
 		serviceMonitorHandler: resources.NewServiceMonitorHandler(client),
 		deltaProcessor:        processors.NewDeltaProcessor(),
 	}
 }
 
-// TODO remove this reconcile loop in future versions
-func (r *KserveMetricsServiceMonitorReconciler) Reconcile(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) error {
+func (r *KserveRawMetricsServiceMonitorReconciler) Reconcile(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) error {
 	log.V(1).Info("Reconciling Metrics ServiceMonitor for InferenceService")
 
 	// Create Desired resource
-	desiredResource, err := r.createDesiredResource(isvc)
+	desiredResource, err := r.createDesiredResource(ctx, log, isvc)
 	if err != nil {
 		return err
 	}
@@ -69,16 +71,43 @@ func (r *KserveMetricsServiceMonitorReconciler) Reconcile(ctx context.Context, l
 	return nil
 }
 
-// TODO remove this reconcile loop in future versions
-func (r *KserveMetricsServiceMonitorReconciler) createDesiredResource(isvc *kservev1beta1.InferenceService) (*v1.ServiceMonitor, error) {
-	return nil, nil
+func (r *KserveRawMetricsServiceMonitorReconciler) createDesiredResource(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) (*v1.ServiceMonitor, error) {
+	isvcRuntime, err := utils.FindSupportingRuntimeForISvc(ctx, r.client, log, isvc)
+	if err != nil {
+		return nil, err
+	}
+
+	desiredServiceMonitor := &v1.ServiceMonitor{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      getMetricsServiceMonitorName(isvc),
+			Namespace: isvc.Namespace,
+		},
+		Spec: v1.ServiceMonitorSpec{
+			Endpoints: []v1.Endpoint{
+				{
+					Port:   isvcRuntime.Name + "-metrics",
+					Scheme: "http",
+				},
+			},
+			NamespaceSelector: v1.NamespaceSelector{},
+			Selector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"name": getMetricsServiceMonitorName(isvc),
+				},
+			},
+		},
+	}
+	if err := ctrl.SetControllerReference(isvc, desiredServiceMonitor, r.client.Scheme()); err != nil {
+		return nil, err
+	}
+	return desiredServiceMonitor, nil
 }
 
-func (r *KserveMetricsServiceMonitorReconciler) getExistingResource(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) (*v1.ServiceMonitor, error) {
+func (r *KserveRawMetricsServiceMonitorReconciler) getExistingResource(ctx context.Context, log logr.Logger, isvc *kservev1beta1.InferenceService) (*v1.ServiceMonitor, error) {
 	return r.serviceMonitorHandler.FetchServiceMonitor(ctx, log, types.NamespacedName{Name: getMetricsServiceMonitorName(isvc), Namespace: isvc.Namespace})
 }
 
-func (r *KserveMetricsServiceMonitorReconciler) processDelta(ctx context.Context, log logr.Logger, desiredServiceMonitor *v1.ServiceMonitor, existingServiceMonitor *v1.ServiceMonitor) (err error) {
+func (r *KserveRawMetricsServiceMonitorReconciler) processDelta(ctx context.Context, log logr.Logger, desiredServiceMonitor *v1.ServiceMonitor, existingServiceMonitor *v1.ServiceMonitor) (err error) {
 	comparator := comparators.GetServiceMonitorComparator()
 	delta := r.deltaProcessor.ComputeDelta(comparator, desiredServiceMonitor, existingServiceMonitor)
 

internal/controller/utils/utils.go
@@ -369,7 +369,7 @@ func SetAvailableResourcesForApi(groupVersion string, resources *metav1.APIResou
 func FindSupportingRuntimeForISvc(ctx context.Context, cli client.Client, log logr.Logger, isvc *kservev1beta1.InferenceService) (*kservev1alpha1.ServingRuntime, error) {
 	desiredServingRuntime := &kservev1alpha1.ServingRuntime{}
 
-	if isvc.Spec.Predictor.Model.Runtime != nil {
+	if isvc.Spec.Predictor.Model != nil && isvc.Spec.Predictor.Model.Runtime != nil {
 		err := cli.Get(ctx, types.NamespacedName{
 			Name:      *isvc.Spec.Predictor.Model.Runtime,
 			Namespace: isvc.Namespace,

internal/controller/serving/inferenceservice_controller_test.go
@@ -32,6 +32,7 @@ import (
 	. "github.com/onsi/gomega/gstruct"
 	gomegatypes "github.com/onsi/gomega/types"
 	routev1 "github.com/openshift/api/route/v1"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	istioclientv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
 	istiosecv1b1 "istio.io/client-go/pkg/apis/security/v1beta1"
 	corev1 "k8s.io/api/core/v1"
@@ -643,7 +644,7 @@ var _ = Describe("InferenceService Controller", func() {
 			return servingRuntime
 		}
 
-		createInferenceService := func(namespace, name string, path string) *kservev1beta1.InferenceService {
+		createInferenceService := func(namespace, name string, path string, isRaw ...bool) *kservev1beta1.InferenceService {
 			inferenceService := &kservev1beta1.InferenceService{}
 			err := testutils.ConvertToStructuredResource(path, inferenceService)
 			Expect(err).NotTo(HaveOccurred())
@@ -651,55 +652,38 @@ var _ = Describe("InferenceService Controller", func() {
 			if len(name) != 0 {
 				inferenceService.Name = name
 			}
+			raw := len(isRaw) > 0 && isRaw[0]
+			if raw {
+				if inferenceService.Annotations == nil {
+					inferenceService.Annotations = map[string]string{}
+				}
+				inferenceService.Annotations["serving.kserve.io/deploymentMode"] = "RawDeployment"
+			}
 			if err := k8sClient.Create(ctx, inferenceService); err != nil && !k8sErrors.IsAlreadyExists(err) {
 				Fail(err.Error())
 			}
 			return inferenceService
 		}
 
-		BeforeEach(func() {
-			testNs = testutils.Namespaces.Create(ctx, k8sClient).Name
-
-			inferenceServiceConfig := &corev1.ConfigMap{}
-			Expect(testutils.ConvertToStructuredResource(InferenceServiceConfigPath1, inferenceServiceConfig)).To(Succeed())
-			if err := k8sClient.Create(ctx, inferenceServiceConfig); err != nil && !k8sErrors.IsAlreadyExists(err) {
-				Fail(err.Error())
-			}
-		})
-
-		When("deploying a Kserve model", func() {
-			It("if the runtime is supported for metrics, it should create a configmap with prometheus queries", func() {
-				_ = createServingRuntime(testNs, KserveServingRuntimePath1)
-				_ = createInferenceService(testNs, KserveOvmsInferenceServiceName, KserveInferenceServicePath1)
-
-				metricsConfigMap, err := testutils.WaitForConfigMap(k8sClient, testNs, KserveOvmsInferenceServiceName+constants.KserveMetricsConfigMapNameSuffix, 30, 1*time.Second)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(metricsConfigMap).NotTo(BeNil())
-
-				finaldata := utils.SubstituteVariablesInQueries(constants.OvmsMetricsData, testNs, KserveOvmsInferenceServiceName)
-				expectedMetricsConfigMap := &corev1.ConfigMap{
+		verifyConfigMap := func(isvcName string, namespace string, supported bool, metricsData string) {
+			metricsConfigMap, err := testutils.WaitForConfigMap(k8sClient, namespace, isvcName+constants.KserveMetricsConfigMapNameSuffix, 30, 1*time.Second)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(metricsConfigMap).NotTo(BeNil())
+			var expectedMetricsConfigMap *corev1.ConfigMap
+			if supported {
+				finaldata := utils.SubstituteVariablesInQueries(metricsData, namespace, isvcName)
+				expectedMetricsConfigMap = &corev1.ConfigMap{
 					ObjectMeta: metav1.ObjectMeta{
-						Name:      KserveOvmsInferenceServiceName + constants.KserveMetricsConfigMapNameSuffix,
-						Namespace: testNs,
+						Name:      isvcName + constants.KserveMetricsConfigMapNameSuffix,
+						Namespace: namespace,
 					},
 					Data: map[string]string{
 						"supported": "true",
 						"metrics":   finaldata,
 					},
 				}
-				Expect(testutils.CompareConfigMap(metricsConfigMap, expectedMetricsConfigMap)).Should(BeTrue())
-				Expect(expectedMetricsConfigMap.Data).NotTo(HaveKeyWithValue("metrics", ContainSubstring("${REQUEST_RATE_INTERVAL}")))
-			})
-
-			It("if the runtime is not supported for metrics, it should create a configmap with the unsupported config", func() {
-				_ = createServingRuntime(testNs, UnsupprtedMetricsServingRuntimePath)
-				_ = createInferenceService(testNs, UnsupportedMetricsInferenceServiceName, UnsupportedMetricsInferenceServicePath)
-
-				metricsConfigMap, err := testutils.WaitForConfigMap(k8sClient, testNs, UnsupportedMetricsInferenceServiceName+constants.KserveMetricsConfigMapNameSuffix, 30, 1*time.Second)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(metricsConfigMap).NotTo(BeNil())
-
-				expectedMetricsConfigMap := &corev1.ConfigMap{
+			} else {
+				expectedMetricsConfigMap = &corev1.ConfigMap{
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      UnsupportedMetricsInferenceServiceName + constants.KserveMetricsConfigMapNameSuffix,
 						Namespace: testNs,
@@ -708,50 +692,79 @@ var _ = Describe("InferenceService Controller", func() {
 						"supported": "false",
 					},
 				}
-				Expect(testutils.CompareConfigMap(metricsConfigMap, expectedMetricsConfigMap)).Should(BeTrue())
+			}
+			Expect(testutils.CompareConfigMap(metricsConfigMap, expectedMetricsConfigMap)).Should(BeTrue())
+			Expect(expectedMetricsConfigMap.Data).NotTo(HaveKeyWithValue("metrics", ContainSubstring("${REQUEST_RATE_INTERVAL}")))
+		}
+
+		BeforeEach(func() {
+			testNs = testutils.Namespaces.Create(ctx, k8sClient).Name
+
+			inferenceServiceConfig := &corev1.ConfigMap{}
+			Expect(testutils.ConvertToStructuredResource(InferenceServiceConfigPath1, inferenceServiceConfig)).To(Succeed())
+			if err := k8sClient.Create(ctx, inferenceServiceConfig); err != nil {
+				Fail(err.Error())
+			}
+		})
+
+		When("deploying a Kserve model", func() {
+			It("[serverless] if the runtime is supported for metrics, it should create a configmap with prometheus queries", func() {
+				_ = createServingRuntime(testNs, KserveServingRuntimePath1)
+				_ = createInferenceService(testNs, KserveOvmsInferenceServiceName, KserveInferenceServicePath1)
+
+				verifyConfigMap(KserveOvmsInferenceServiceName, testNs, true, constants.OvmsMetricsData)
 			})
 
-			It("if the isvc does not have a runtime specified, an unsupported metrics configmap should be created", func() {
+			It("[raw] if the runtime is supported for metrics, it should create a configmap with prometheus queries", func() {
+				_ = createServingRuntime(testNs, KserveServingRuntimePath1)
+				_ = createInferenceService(testNs, KserveOvmsInferenceServiceName, KserveInferenceServicePath1, true)
+
+				verifyConfigMap(KserveOvmsInferenceServiceName, testNs, true, constants.OvmsMetricsData)
+			})
+
+			It("[serverless] if the runtime is not supported for metrics, it should create a configmap with the unsupported config", func() {
+				_ = createServingRuntime(testNs, UnsupprtedMetricsServingRuntimePath)
+				_ = createInferenceService(testNs, UnsupportedMetricsInferenceServiceName, UnsupportedMetricsInferenceServicePath)
+
+				verifyConfigMap(UnsupportedMetricsInferenceServiceName, testNs, false, "")
+			})
+
+			It("[raw] if the runtime is not supported for metrics, it should create a configmap with the unsupported config", func() {
+				_ = createServingRuntime(testNs, UnsupprtedMetricsServingRuntimePath)
+				_ = createInferenceService(testNs, UnsupportedMetricsInferenceServiceName, UnsupportedMetricsInferenceServicePath, true)
+
+				verifyConfigMap(UnsupportedMetricsInferenceServiceName, testNs, false, "")
+			})
+
+			It("[serverless] if the isvc does not have a runtime specified and there is no supported runtime, an unsupported metrics configmap should be created", func() {
 				_ = createInferenceService(testNs, NilRuntimeInferenceServiceName, NilRuntimeInferenceServicePath)
 
-				metricsConfigMap, err := testutils.WaitForConfigMap(k8sClient, testNs, NilRuntimeInferenceServiceName+constants.KserveMetricsConfigMapNameSuffix, 30, 1*time.Second)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(metricsConfigMap).NotTo(BeNil())
-
-				expectedmetricsConfigMap := &corev1.ConfigMap{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      NilRuntimeInferenceServiceName + constants.KserveMetricsConfigMapNameSuffix,
-						Namespace: testNs,
-					},
-					Data: map[string]string{
-						"supported": "false",
-					},
-				}
-				Expect(testutils.CompareConfigMap(metricsConfigMap, expectedmetricsConfigMap)).Should(BeTrue())
+				verifyConfigMap(NilRuntimeInferenceServiceName, testNs, false, "")
 			})
 
-			It("if the isvc does not have the model field specified, an unsupported metrics configmap should be created", func() {
+			It("[raw] if the isvc does not have a runtime specified and there is no supported runtime, an unsupported metrics configmap should be created", func() {
+				_ = createInferenceService(testNs, NilRuntimeInferenceServiceName, NilRuntimeInferenceServicePath, true)
+
+				verifyConfigMap(NilRuntimeInferenceServiceName, testNs, false, "")
+			})
+
+			It("[serverless] if the isvc does not have the model field specified, an unsupported metrics configmap should be created", func() {
 				_ = createInferenceService(testNs, NilModelInferenceServiceName, NilModelInferenceServicePath)
 
-				metricsConfigMap, err := testutils.WaitForConfigMap(k8sClient, testNs, NilModelInferenceServiceName+constants.KserveMetricsConfigMapNameSuffix, 30, 1*time.Second)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(metricsConfigMap).NotTo(BeNil())
+				verifyConfigMap(NilModelInferenceServiceName, testNs, false, "")
+			})
 
-				expectedCM := &corev1.ConfigMap{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      NilModelInferenceServiceName + constants.KserveMetricsConfigMapNameSuffix,
-						Namespace: testNs,
-					},
-					Data: map[string]string{
-						"supported": "false",
-					},
-				}
-				Expect(testutils.CompareConfigMap(metricsConfigMap, expectedCM)).Should(BeTrue())
+			It("[raw] if the isvc does not have the model field specified, an unsupported metrics configmap should be created", func() {
+				_ = createInferenceService(testNs, NilModelInferenceServiceName, NilModelInferenceServicePath, true)
+
+				verifyConfigMap(NilModelInferenceServiceName, testNs, false, "")
 			})
 		})
 
 		When("deleting the deployed models", func() {
-			It("it should delete the associated configmap", func() {
+			timeout10s := time.Second * 10
+			interval4s := time.Second * 4
+			It("[serverless] it should delete the associated configmap", func() {
 				_ = createServingRuntime(testNs, KserveServingRuntimePath1)
 				OvmsInferenceService := createInferenceService(testNs, KserveOvmsInferenceServiceName, KserveInferenceServicePath1)
 
@@ -761,7 +774,7 @@ var _ = Describe("InferenceService Controller", func() {
 					key := types.NamespacedName{Name: KserveOvmsInferenceServiceName + constants.KserveMetricsConfigMapNameSuffix, Namespace: OvmsInferenceService.Namespace}
 					err := k8sClient.Get(ctx, key, configmap)
 					return err
-				}, timeout, interval).ShouldNot(Succeed())
+				}, timeout10s, interval4s).ShouldNot(Succeed())
 
 				_ = createServingRuntime(testNs, UnsupprtedMetricsServingRuntimePath)
 				SklearnInferenceService := createInferenceService(testNs, UnsupportedMetricsInferenceServiceName, UnsupportedMetricsInferenceServicePath)
@@ -772,7 +785,30 @@ var _ = Describe("InferenceService Controller", func() {
 					key := types.NamespacedName{Name: UnsupportedMetricsInferenceServiceName + constants.KserveMetricsConfigMapNameSuffix, Namespace: SklearnInferenceService.Namespace}
 					err := k8sClient.Get(ctx, key, configmap)
 					return err
-				}, timeout, interval).ShouldNot(Succeed())
+				}, timeout10s, interval4s).ShouldNot(Succeed())
+			})
+			It("[raw] it should delete the associated configmap", func() {
+				_ = createServingRuntime(testNs, KserveServingRuntimePath1)
+				OvmsInferenceService := createInferenceService(testNs, KserveOvmsInferenceServiceName, KserveInferenceServicePath1, true)
+
+				Expect(k8sClient.Delete(ctx, OvmsInferenceService)).Should(Succeed())
+				Eventually(func() error {
+					configmap := &corev1.ConfigMap{}
+					key := types.NamespacedName{Name: KserveOvmsInferenceServiceName + constants.KserveMetricsConfigMapNameSuffix, Namespace: OvmsInferenceService.Namespace}
+					err := k8sClient.Get(ctx, key, configmap)
+					return err
+				}, timeout10s, interval4s).ShouldNot(Succeed())
+
+				_ = createServingRuntime(testNs, UnsupprtedMetricsServingRuntimePath)
+				SklearnInferenceService := createInferenceService(testNs, UnsupportedMetricsInferenceServiceName, UnsupportedMetricsInferenceServicePath, true)
+
+				Expect(k8sClient.Delete(ctx, SklearnInferenceService)).Should(Succeed())
+				Eventually(func() error {
+					configmap := &corev1.ConfigMap{}
+					key := types.NamespacedName{Name: UnsupportedMetricsInferenceServiceName + constants.KserveMetricsConfigMapNameSuffix, Namespace: SklearnInferenceService.Namespace}
+					err := k8sClient.Get(ctx, key, configmap)
+					return err
+				}, timeout10s, interval4s).ShouldNot(Succeed())
 			})
 		})
 	})
@@ -1093,6 +1129,26 @@ var _ = Describe("InferenceService Controller", func() {
 					return k8sClient.Get(ctx, key, route)
 				}, timeout, interval).ShouldNot(HaveOccurred())
 			})
+			It("it should create a metrics service and servicemonitor auth", func() {
+				_ = createServingRuntime(testNs, KserveServingRuntimePath1)
+				inferenceService := createInferenceService(testNs, KserveOvmsInferenceServiceName, KserveInferenceServicePath1)
+				if err := k8sClient.Create(ctx, inferenceService); err != nil {
+					Expect(err).NotTo(HaveOccurred())
+				}
+				metricsService := &corev1.Service{}
+				Eventually(func() error {
+					key := types.NamespacedName{Name: inferenceService.Name + "-metrics", Namespace: inferenceService.Namespace}
+					return k8sClient.Get(ctx, key, metricsService)
+				}, timeout, interval).Should(Succeed())
+
+				serviceMonitor := &monitoringv1.ServiceMonitor{}
+				Eventually(func() error {
+					key := types.NamespacedName{Name: inferenceService.Name + "-metrics", Namespace: inferenceService.Namespace}
+					return k8sClient.Get(ctx, key, serviceMonitor)
+				}, timeout, interval).Should(Succeed())
+
+				Expect(serviceMonitor.Spec.Selector.MatchLabels).To(HaveKeyWithValue("name", inferenceService.Name+"-metrics"))
+			})
 		})
 		When("deleting a Kserve RawDeployment model", func() {
 			It("the associated route should be deleted", func() {
@@ -1108,6 +1164,27 @@ var _ = Describe("InferenceService Controller", func() {
 				Eventually(func() error {
 					key := types.NamespacedName{Name: inferenceService.Name, Namespace: inferenceService.Namespace}
 					return k8sClient.Get(ctx, key, route)
+				}, timeout, interval).Should(HaveOccurred())
+			})
+			It("the associated metrics service and servicemonitor should be deleted", func() {
+				_ = createServingRuntime(testNs, KserveServingRuntimePath1)
+				inferenceService := createInferenceService(testNs, KserveOvmsInferenceServiceName, KserveInferenceServicePath1)
+				if err := k8sClient.Create(ctx, inferenceService); err != nil {
+					Expect(err).NotTo(HaveOccurred())
+				}
+
+				Expect(k8sClient.Delete(ctx, inferenceService)).Should(Succeed())
+
+				metricsService := &corev1.Service{}
+				Eventually(func() error {
+					key := types.NamespacedName{Name: inferenceService.Name, Namespace: inferenceService.Namespace}
+					return k8sClient.Get(ctx, key, metricsService)
+				}, timeout, interval).Should(HaveOccurred())
+
+				serviceMonitor := &monitoringv1.ServiceMonitor{}
+				Eventually(func() error {
+					key := types.NamespacedName{Name: inferenceService.Name, Namespace: inferenceService.Namespace}
+					return k8sClient.Get(ctx, key, serviceMonitor)
 				}, timeout, interval).Should(HaveOccurred())
 			})
 		})

commit b3e6c0b4dce63082f7976504a7f3c364eb6548cb

fix: skip runtime auto-selection for raw container InferenceServices
Raw container InferenceServices (e.g. message-dumper) have no Model or
ModelFormat - they use a plain container spec with no serving runtime.
FindSupportingRuntimeForISvc did not account for this and panicked on
nil Model dereference when the metrics reconcilers tried to auto-select
a runtime.

Returns nil from FindSupportingRuntimeForISvc when there is no Model
or ModelFormat to match against, and handles nil runtime gracefully in
the metrics sub-reconcilers.

The bug was introduced in 203969f3 (Jan 2025) when metrics reconcilers
were added to the raw deployment path. It manifests as an intermittent
e2e flake - repeated recovered panics can starve the leader lease
renewal, restarting the pod and briefly leaving the webhook unavailable.

e2e failure: https://prow.ci.openshift.org/view/gs/test-platform-results/pr-logs/pull/opendatahub-io_kserve/1554/pull-ci-opendatahub-io-kserve-master-e2e-raw/2061489406899916800#1:build-log.txt%3A1669
controller logs: https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/test-platform-results/pr-logs/pull/opendatahub-io_kserve/1554/pull-ci-opendatahub-io-kserve-master-e2e-raw/2061489406899916800/artifacts/e2e-raw/openshift-must-gather/artifacts/gather-openshift/quay-io-openshift-release-dev-ocp-v4-0-art-dev-sha256-11ec132408bc65a929187701d132562d5b609813a819218e55fc672af97864b9/namespaces/kserve/pods/odh-model-controller-696fc77849-qxc8m/manager/manager/logs/

Signed-off-by: Bartosz Majsak <bartosz.majsak@gmail.com>

internal/controller/serving/reconcilers/kserve_metrics_service_reconciler.go
@@ -81,7 +81,7 @@ func (r *KserveRawMetricsServiceReconciler) createDesiredResource(ctx context.Co
 		return nil, err
 	}
 
-	if isvcRuntime.Spec.Annotations == nil || isvcRuntime.Spec.Annotations[constants.PrometheusPortAnnotationKey] == "" {
+	if isvcRuntime == nil || isvcRuntime.Spec.Annotations == nil || isvcRuntime.Spec.Annotations[constants.PrometheusPortAnnotationKey] == "" {
 		log.V(1).Info("No Prometheus annotations on ServingRuntime, skipping creation of metrics resources")
 		return nil, nil
 	}

internal/controller/serving/reconcilers/kserve_metrics_servicemonitor_reconciler.go
@@ -82,6 +82,10 @@ func (r *KserveRawMetricsServiceMonitorReconciler) createDesiredResource(ctx con
 		return nil, err
 	}
 
+	if isvcRuntime == nil {
+		return nil, nil
+	}
+
 	endpoint := v1.Endpoint{
 		Port:   isvcRuntime.Name + "-metrics",
 		Scheme: ptr.To(v1.Scheme("http")),

internal/controller/utils/utils.go
@@ -162,6 +162,10 @@ func FindSupportingRuntimeForISvc(ctx context.Context, cli client.Client, log lo
 		}
 		return desiredServingRuntime, nil
 	} else {
+		if isvc.Spec.Predictor.Model == nil || isvc.Spec.Predictor.Model.ModelFormat.Name == "" {
+			return nil, nil
+		}
+
 		runtimes := &kservev1alpha1.ServingRuntimeList{}
 		err := cli.List(ctx, runtimes, client.InNamespace(isvc.Namespace))
 		if err != nil {